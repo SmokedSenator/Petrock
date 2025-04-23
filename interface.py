50c8cbc3347160210d173adfce0e26b1ba9779f8
SmokedSenator <o0z0o@yandex.ru>
2025-04-23 22:19:42 +0300

    Visual on flask start to exist

--- interface.py
+++ interface.py
@@ -1,2 +1,11 @@
-import pandas
-import django
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def index():
+    return "Welcome to the starters"
+
+if __name__ == "interface":
+    app.run(debug=True)
